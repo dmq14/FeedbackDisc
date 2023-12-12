b94fb49abbc64d484f54db23f7dc92aaadf8735a
dmq14 <dangminhquang1401@gmail.com>
2023-12-12 15:31:23 +0700

    update folder bot & server

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const {Client, GatewayIntentBits, Collection} = require('discord.js');
+const mongoose = require("mongoose");
 const command = require("./handlers/command");
 
 const client = new Client({
@@ -10,5 +11,11 @@ client.command = new Collection();
 client.countdown = new Collection();
 
 ["event","command"].forEach((file) => require(`./handlers/${file}`)(client));
+if(process.env.MONGO){
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("Connect mongo!")
+    });
+    mongoose.connection.on("disconnected",()=> console.log("Disconnect mongo!!"))
+}
 
 client.login(process.env.TOKEN);
