ff62f47fb8315f92e8ddbcd1e7fdd650fc2ec659
dmq14 <dangminhquang1401@gmail.com>
2023-12-12 14:16:33 +0700

    command + event for ping

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
-const {Client, GatewayIntentBits} = require('discord.js');
+const {Client, GatewayIntentBits, Collection} = require('discord.js');
+const command = require("./handlers/command");
+
 const client = new Client({
     intents : [GatewayIntentBits.Guilds]
 });
+
+client.command = new Collection();
+client.countdown = new Collection();
+
+["event","command"].forEach((file) => require(`./handlers/${file}`)(client));
 
 client.login(process.env.TOKEN);
