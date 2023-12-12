85ff78b20c5ac15ccfa2ec59dcc63e4c46e30ad7
dmq14 <dangminhquang1401@gmail.com>
2023-12-12 10:57:24 +0700

    turn on bot

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const {Client, GatewayIntentBits} = require('discord.js');
+const client = new Client({
+    intents : [GatewayIntentBits.Guilds]
+});
+
+client.login(process.env.TOKEN);
